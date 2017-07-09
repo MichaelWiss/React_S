57be4fbccf56aa09a381dd1ccf5e4a0f057ce8e5
MichaelWiss <michael.wiss@gmail.com>
2017-07-09 09:21:53 -0500

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ module.exports = {
 	entry: './app/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'my-first-webpack.bundle.js'
-	}
+		filename: 'index_bundle.js'
+	},
+	module: {
+		rules: [
+		{ test: /\.(js)$/, use: 'babel-loader' },
+        { test: /\.css$/, use: [ 'style-loader', 'css-loader']}
+		]
+	},
 }
