095876acc9aaf6e44dfe4cc5a69ed6301117aef8
MichaelWiss <michael.wiss@gmail.com>
2017-07-09 08:57:40 -0500

    webpack.config

--- app/index.js
+++ app/index.js
@@ -1,3 +1,20 @@
 var React = require('react');
 var ReactDOM = ('react-dom');
 
+require('./index.css');
+
+
+class App extends React.component {
+	render () {
+		return (
+			<div>
+			 Hello World!
+			 </div>
+	    )
+	}
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementBYID('app')
+);
